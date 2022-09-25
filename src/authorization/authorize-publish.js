a15842ab3a1ccd0313618605b8181154d071dd19
Henrique Bonfá Custódio <henriquebcustodio@outlook.com>
2022-09-25 19:14:34 -0300

    Add publish authorization logic

--- src/authorization/authorize-publish.js
+++ src/authorization/authorize-publish.js
@@ -1,4 +1,12 @@
 const authorizePublish = (client, packet, callback) => {
+  const pattern = new RegExp(`\/devices\/${client.topic_id}`)
+  const match = packet.topic.match(pattern)
+
+  if (!match) {
+    const error = new Error(`This device can only publish to /devices/${client.topic_id}`)
+    return callback(error)
+  }
+
   callback(null);
 };
 
